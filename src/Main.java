e41d4a3310027c3a030577c20d4db906d769d6f5
Natalia Zubareva <n.zubareva@inbox.ru>
2022-05-02 21:49:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("101", "0011"));
+        System.out.println(bins.mult("110", "101"));
+
+    }
+
+}
